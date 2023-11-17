84a9e1fb1e9d580fe70f07fad60333021167aead
Shahir47 <shahir.glhd@gmail.com>
2023-11-17 00:04:20 -0600

    Local first push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Land from './components/Land.jsx';
+import { History } from './components/History.jsx';
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Land />
+      <History />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/History.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const History = () => {
+  return (
+    <div className='w-full h-screen'>
+        <p>Hello</p>
+    </div>
+  )
+}
