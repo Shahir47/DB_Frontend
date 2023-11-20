397c38fd7259eeec2722bfb077d1ce1d1ad9a61e
Shahir47 <shahir.glhd@gmail.com>
2023-11-19 19:31:52 -0600

    Tooltip in navbar

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -5,20 +5,28 @@ import { GiSoccerField } from "react-icons/gi";
 const SideNav = () => {
   return (
     <div className='w-[70px] h-screen bg-[#0d406ae3] fixed'>
-        <div className='flex items-end tracking-widest'>
-            <h1 className='ml-[10px] text-white'>S</h1>
-            <h2 className='text-[#042138e3]'><span className='text-white mr-[3px]'>o</span>ccer</h2>
-        </div>
-        <div className='flex items-end tracking-widest'>
-            <h1 className='ml-[30px] text-white'>P</h1>
-            <h2 className='ml-[3px] text-[#042138e3]'>redict</h2>
-        </div>
+        <a href='#hero'>
+            <div className='flex items-end tracking-widest'>
+                <h1 className='ml-[10px] text-white'>S</h1>
+                <h2 className='text-[#042138e3]'><span className='text-white mr-[3px]'>o</span>ccer</h2>
+            </div>
+            <div className='flex items-end tracking-widest'>
+                <h1 className='ml-[30px] text-white'>P</h1>
+                <h2 className='ml-[3px] text-[#042138e3]'>redict</h2>
+            </div>
+        </a>
         <div className='w-[50px] mx-auto h-[60%] flex flex-col items-center justify-center'>
             <a href="#hero" >
-                <GiSoccerBall className='my-[60px] text-white' size={40}/>
+                <div className='tooltip my-[60px]'>
+                    <GiSoccerBall className=' text-white' size={40}/>
+                    <span className='tooltip-text px-2'>Home</span>
+                </div>
             </a>
             <a href="#hist">
-                <GiSoccerField className='text-white' size={40}/>
+                <div className='tooltip'>
+                    <GiSoccerField className='text-white' size={40}/>
+                    <span className='tooltip-text px-2'>History</span>
+                </div>
             </a>
         </div>
     </div>
