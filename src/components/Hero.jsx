463e92fb41fefcf119febb88031d5f2127d09e10
Shahir47 <shahir.glhd@gmail.com>
2023-11-19 22:23:22 -0600

    Hero page progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,39 @@ import React from 'react'
 const Hero = () => {
   return (
     <div id="hero" className='w-full h-screen'>
-        <div className='max-w-[1240px] mx-auto'>
-            Hero
+        <div className='max-w-[1240px] mx-auto p-1'>
+            <div className='mt-[100px] mb-[150px]'>
+                <h2 className='text-center'>Welcome to Soccer Predict</h2>
+                <p className='text-center uppercase pt-2 tracking-wider'>A platform to predict the score of incoming soccer matches.</p>
+            </div>
+            <div className='max-w-[1050px] mx-auto'>
+                <h3>Let's Predict</h3>
+                <p className='text-sm tracking-wider'>Enter the information about the match you want to predict</p>
+                <div className='mt-[25px]'>
+                    <form action="">
+                        <div className='grid grid-cols-3 gap-8'>
+                            <div className='flex flex-col'>
+                                <label className='text-lg' for='home_team'>Home Team</label>
+                                <input className='text-[#021321e3] px-2 h-[45px] rounded-lg border-gray-300 border-[2px] focus:outline-[2px] outline-gray-400' id='home_team' type='text' name='home_team'/>
+                            </div>
+                            <div className='flex flex-col'>
+                                <label className='text-lg' for='away_team'>Away Team</label>
+                                <input className='text-[#021321e3] px-2 h-[45px] rounded-lg border-gray-300 border-[2px] focus:outline-[2px] outline-gray-400' id='away_team' type='text' name='away_team'/>
+                            </div>
+                            <div className='flex flex-col'>
+                                <label className='text-lg' for='date'>Date</label>
+                                <input className='text-[#021321e3] px-2 h-[45px] rounded-lg border-gray-300 border-[2px] focus:outline-[2px] outline-gray-400' id='date' type='date' name='date'/>
+                            </div>
+
+                            <div></div>
+                            <div className='mx-auto '>
+                                <button className='rounded-lg text-lg bg-[#0d406ae3] text-white border-2 border-[#0d406a] h-[45px] w-[200px] flex justify-center items-center'>Submit</button>
+                            </div>
+                            <div></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
   )
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -6,13 +6,13 @@ const SideNav = () => {
   return (
     <div className='w-[70px] h-screen bg-[#0d406ae3] fixed'>
         <a href='#hero'>
-            <div className='flex items-end tracking-widest'>
-                <h1 className='ml-[10px] text-white'>S</h1>
-                <h2 className='text-[#042138e3]'><span className='text-white mr-[3px]'>o</span>ccer</h2>
+            <div className='flex items-end tracking-widest font_logo'>
+                <h1 className='text-6xl ml-[14px] text-white'>S</h1>
+                <h2 className='text-4xl text-[#042138e3]'><span className='text-white mr-[3px]'>o</span>ccer</h2>
             </div>
-            <div className='flex items-end tracking-widest'>
-                <h1 className='ml-[30px] text-white'>P</h1>
-                <h2 className='ml-[3px] text-[#042138e3]'>redict</h2>
+            <div className='flex items-end tracking-widest font_logo'>
+                <h1 className='text-6xl ml-[29px] text-white'>P</h1>
+                <h2 className='text-4xl ml-[3px] text-[#042138e3]'>redict</h2>
             </div>
         </a>
         <div className='w-[50px] mx-auto h-[60%] flex flex-col items-center justify-center'>
